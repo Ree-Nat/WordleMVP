18d9c1b2bc8ea71735f2a8b50e1df93e9a43808d
Nathan <53132424+Ree-Nat@users.noreply.github.com>
2025-09-23 13:11:58 -0700

    swap key and value in valid wordle words

--- src/main/java/Model/WordFetcher.java
+++ src/main/java/Model/WordFetcher.java
@@ -1,4 +1,22 @@
 package Model;
+import java.util.Random;
 
 public class WordFetcher {
+
+    public WordFetcher()
+    {
+
+
+    }
+
+    Boolean exists()
+    {
+
+    }
+
+    String getRandomWord()
+    {
+        return "";
+    }
+
 }
